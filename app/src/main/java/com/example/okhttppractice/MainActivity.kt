a674ee4a5c947c16da36c74b851ef46e8116aabf
SmillingMonster <rukokuro@gmail.com>
2023-01-17 16:21:21 +0200

    PUT, DELETE, PATCH practice

--- app/src/main/java/com/example/okhttppractice/MainActivity.kt
+++ app/src/main/java/com/example/okhttppractice/MainActivity.kt
@@ -11,16 +11,23 @@ class MainActivity : AppCompatActivity() {
     //https://www.digitalocean.com/community/tutorials/okhttp-android-example-tutorial
 
     //GET
-    var url = "https://reqres.in/api/users/2"
+    private var url = "https://reqres.in/api/users/2"
 
     //POST
-    var postUrl = "https://reqres.in/api/users/"
-    var postBody = """{
+    private var postUrl = "https://reqres.in/api/users/"
+    private var postBody = """{
     "name": "morpheus",
     "job": "leader"
 }"""
 
-    val JSON: MediaType = MediaType.parse("application/json; charset=utf-8")!!
+    //POST
+    private var putUrl = "https://reqres.in/api/users/2"
+    private var putBody = """{
+    "name": "morpheus",
+    "job": "zion resident"
+}"""
+
+    private val JSON: MediaType = MediaType.parse("application/json; charset=utf-8")!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +46,27 @@ class MainActivity : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+
+        //PUT
+        try {
+            putRequest(putUrl, putBody)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        //PUT
+        try {
+            deleteRequest(putUrl, putBody)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        //PATCH
+        try {
+            patchRequest(putUrl, putBody)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
     //GET
@@ -47,17 +75,18 @@ class MainActivity : AppCompatActivity() {
         val client = OkHttpClient()
         val request = Request.Builder().url(url).build()
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException?) {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("mLog", "GET Error: $e")
                 call.cancel()
             }
 
             @Throws(IOException::class)
-            override fun onResponse(call: Call?, response: Response) {
+            override fun onResponse(call: Call, response: Response) {
                 val myResponse = response.body()!!.string()
-                Log.i("mLog", "$myResponse")
+                Log.i("mLog", "GET $myResponse")
 
                 val txt = (JSONObject(myResponse).getJSONObject("data").get("avatar")).toString()
-                Log.i("mLog", "$txt")
+                Log.i("mLog", "GET $txt")
 //                runOnUiThread { txtString.setText(myResponse) }
             }
         })
@@ -65,18 +94,76 @@ class MainActivity : AppCompatActivity() {
 
     //POST
     @Throws(IOException::class)
-    fun postRequest(postUrl: String?, postBody: String?) {
+    fun postRequest(postUrl: String, postBody: String) {
         val client = OkHttpClient()
         val body = RequestBody.create(JSON, postBody)
         val request = Request.Builder().url(postUrl).post(body).build()
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException?) {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("mLog", "POST Error: $e")
                 call.cancel()
             }
 
             @Throws(IOException::class)
-            override fun onResponse(call: Call?, response: Response) {
-                Log.i("mLog", "${response.body()!!.string()}")
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("mLog", "POST ${response.body()!!.string()}")
+            }
+        })
+    }
+
+    //PUT
+    @Throws(IOException::class)
+    fun putRequest(postUrl: String, putBody: String) {
+        val client = OkHttpClient()
+        val body = RequestBody.create(JSON, putBody)
+        val request = Request.Builder().url(postUrl).put(body).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("mLog", "PUT Error: $e")
+                call.cancel()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("mLog", "PUT ${response.body()!!.string()}")
+            }
+        })
+    }
+
+    //DELETE
+    @Throws(IOException::class)
+    fun deleteRequest(postUrl: String, postBody: String) {
+        val client = OkHttpClient()
+        val body = RequestBody.create(JSON, postBody)
+        val request = Request.Builder().url(postUrl).delete(body).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("mLog", "DELETE Error: $e")
+                call.cancel()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("mLog", "DELETE ${response.code()}")
+            }
+        })
+    }
+
+    //PATCH
+    @Throws(IOException::class)
+    fun patchRequest(patchUrl: String, patchBody: String) {
+        val client = OkHttpClient()
+        val body = RequestBody.create(JSON, patchBody)
+        val request = Request.Builder().url(patchUrl).patch(body).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("mLog", "PATCH Error: $e")
+                call.cancel()
+            }
+
+            @Throws(IOException::class)
+            override fun onResponse(call: Call, response: Response) {
+                Log.i("mLog", "PATCH ${response.body()!!.string()}")
             }
         })
     }
